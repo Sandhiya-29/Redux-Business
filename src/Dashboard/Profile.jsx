ef588b90fd8478d94b1a06d3ae09b271b3a0e6b6
Sandhiya_292 <sandhiya@market-intellect.com>
2024-10-22 11:32:42 +0530

    files added

--- src/Dashboard/Profile.jsx
+++ src/Dashboard/Profile.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import './Profile.css';
 import { FaUserCircle} from 'react-icons/fa';
 import { LuPencil } from "react-icons/lu";
+import background from './360_F_172318263_046YEZYCK2hDGJR6X6lm4Gbaxar65Rew.jpg';
 
 const Profile = () => {
   return (
@@ -13,7 +14,7 @@ const Profile = () => {
         <div className="profile">
          <div className='profile-container'>
           <div>
-              <div className='background' ></div>
+             <img src={background} alt="background" className='background' />
               <div className='image-container'>
               <FaUserCircle className='profile-image' />
                </div>
@@ -23,13 +24,13 @@ const Profile = () => {
                </div>
               <div className="details">
                 <h2 className="user-name">Sandhiya</h2>
-                <h3 className="user-role">Investor</h3>
-                <h4 className="user-bio">Bio</h4>
+                <p className="user-role">Investor</p>
+                <h5 className="user-bio">Bio</h5>
                 <h3 className="user-number">+91 78907654321</h3>
                 <p className="user-location">Coimbatore,TamilNadu,India</p>
                 <h4 className='user-interest'>Area of Interest</h4>
+                <h3 className="user-experience">Experience</h3>
                 <a href="L#" className="user-links">https://facebook</a>
-               
               </div>
          </div>
           <div className='members-2'>
